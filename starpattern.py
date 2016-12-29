c31f9e02505e0af33d2f8caa209a96dc1ca83e20
naveentechelium <naveen.m@techelium.com>
2016-12-29 09:59:40 +0530

    Create starpattern.py

---
+++ starpattern.py
@@ -0,0 +1,13 @@
+## This is the Module, which iam defining the function with varibles
+
+def star(a,b,c):
+
+    a='*'*125
+    b="Its me Naveen".center(139)
+    c="*"*125
+    print a,b,c
+    
+ ### This is the calling function
+     
+   import starpattern
+   print starpattern.star(1,2,5)
